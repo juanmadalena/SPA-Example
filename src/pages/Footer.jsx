19286a1d804a1268c30b1b35ab8ed6bd8a659cac
Juan Madalena <juanmadalena06@gmail.com>
2023-11-06 13:24:41 +0100

    first commit

---
+++ src/pages/Footer.jsx
@@ -0,0 +1,21 @@
+
+const Footer = () => {
+    
+    return (
+        <footer className="w-full theme-dark footer-0 text-center-desktop text-center-mobile">
+    
+            <ul className="footer-links">
+                <li><a href="/">Home</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/services">Services</a></li>
+            </ul>
+        
+            <div className="copyright">
+                <p>Copyright © 2023 ABC Limited. All rights reserved.</p>
+            </div>
+        </footer>
+   
+    )
+}
+
+export default Footer;
